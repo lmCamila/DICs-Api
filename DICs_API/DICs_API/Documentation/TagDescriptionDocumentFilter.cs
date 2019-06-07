0cbb96672f93905dea01b5176cf29d89c72d3506
Camila Lima Martins <camilalm31@gmail.com>
2019-06-07 13:30:30 -0300

    add pastas models , documentation repositories

---
+++ DICs_API/DICs_API/Documentation/TagDescriptionDocumentFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DICs_API.Documentation
+{
+    public class TagDescriptionDocumentFilter
+    {
+    }
+}
