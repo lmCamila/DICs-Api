86de7e15a4db1b64a3141ebaf173066583a4e636
tamyres <tamy.yu@hotmail.com>
2019-06-10 10:54:41 -0300

    criação do repository e controller de Department

---
+++ DICs_API/DICs_API/Controllers/DepartmentController.cs
@@ -0,0 +1,77 @@
+﻿using DICs_API.Models;
+using DICs_API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DICs_API.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly DepartmentRepository _repoDepartment;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _repoDepartment = new DepartmentRepository(configuration);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute]int id)
+        {
+            var model = _repoDepartment.Get(id);
+            if(model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = _repoDepartment.GetAll();
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _repoDepartment.Insert(department);
+                var lastResult = result ? _repoDepartment.GetLastInserted() : null;
+                var uri = Url.Action("Get", new { Id = lastResult.Id });
+                return Created(uri, lastResult);
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var department = _repoDepartment.Get(id);
+            if(department == null)
+            {
+                return NotFound();
+            }
+            _repoDepartment.Delete(id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult Update([Bind("Id, Name")]Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _repoDepartment.Update(department);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
--- DICs_API/DICs_API/Models/Configuration.cs
+++ DICs_API/DICs_API/Models/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace DICs_API.Models
 {
     public class Configuration
     {
+
         public int Id { get; set; }
+        [Required]
         public int IdPeriod { get; set; }
     }
 }
