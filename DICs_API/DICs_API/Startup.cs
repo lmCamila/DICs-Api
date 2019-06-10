099455631cfab37a20da97f11fb79e5ea531d1a8
Camila Lima Martins <camilalm31@gmail.com>
2019-06-09 21:15:01 -0300

    Configurações de  swagger, versioning

--- DICs_API/DICs_API/Startup.cs
+++ DICs_API/DICs_API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DICs_API.Documentation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DICs_API
 {
@@ -26,6 +28,19 @@ namespace DICs_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Configuração de padronização de erros 
+
+            //Configuração de versionamento da api
+            services.AddApiVersioning();
+
+            //Configuração Swagger
+            services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1", new Info { Title = "Documentação API DICs", Version = "1.0" });
+               c.EnableAnnotations();
+               c.DocumentFilter<TagDescriptionDocumentFilter>();
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +58,13 @@ namespace DICs_API
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "API Planner v1");
+                opt.RoutePrefix = string.Empty;
+            });
         }
     }
 }
