b6f2add7caa2db059e9243bc3d759fe77a9a406f
Camila Lima Martins <camilalm31@gmail.com>
2019-06-10 14:21:10 -0300

    Merge

---
+++ DICs_API/DICs_API/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DICs_API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DICs_API.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly StatusRepository _repoStatus;
+        public StatusController(IConfiguration configuration)
+        {
+            _repoStatus = new StatusRepository(configuration);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = _repoStatus.GetAll();
+            return Ok(list);
+        }
+    }
+}
---
+++ DICs_API/DICs_API/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DICs_API.Models;
+using DICs_API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DICs_API.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersRepository _repoUsers;
+        public UsersController(IConfiguration configuration)
+        {
+            _repoUsers = new UsersRepository(configuration);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute]int id)
+        {
+            var model = _repoUsers.Get(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = _repoUsers.GetAll();
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _repoUsers.Insert(users);
+                var lastResult = result ? _repoUsers.GetLastInserted() : null;
+                var uri = Url.Action("Get", new { Id = lastResult.Id });
+                return Created(uri, lastResult);
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var department = _repoUsers.Get(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            _repoUsers.Delete(id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult Update([Bind("Id, Name, Avatar, Email, Department, Process, IsLeaderDepartment, IsLeaderProcess")]Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                _repoUsers.Update(users);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+
+}
