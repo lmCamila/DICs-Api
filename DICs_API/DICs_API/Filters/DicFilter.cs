8d4af85826ec71b7e5a47b72008e4fbf602ffdc1
tamyres <tamy.yu@hotmail.com>
2019-06-12 09:34:28 -0300

    filter

---
+++ DICs_API/DICs_API/Filters/DicFilter.cs
@@ -0,0 +1,49 @@
+﻿using DICs_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DICs_API.Filters
+{
+    public class DicFilter
+    {
+        public string User { get; set; }
+        public string Department { get; set; }
+        public string Process { get; set; }
+        public string Period { get; set; }
+        public string Status { get; set; }
+        public string Finished { get; set; }
+    }
+
+    public static class DicFilterExtensions
+    {
+        public static IQueryable<DIC>Filter(this IQueryable<DIC> query, DicFilter filter)
+        {
+            if(filter != null)
+            {
+                if (!string.IsNullOrEmpty(filter.User))
+                {
+                    query = query.Where(d => d.User.Name.ToLower().Contains(d.User.Name.ToLower()));
+                }
+                if (!string.IsNullOrEmpty(filter.Department))
+                {
+                    query = query.Where(d => d.User.Department.Name.ToLower().Contains(d.User.Department.Name.ToLower()));
+                }
+                if (!string.IsNullOrEmpty(filter.Process))
+                {
+                    query = query.Where(d => d.User.Process.Name.ToLower().Contains(d.User.Process.Name.ToLower()));
+                }
+                if (!string.IsNullOrEmpty(filter.Period))
+                {
+                    query = query.Where(d => d.Period.Name.ToLower().Contains(d.Period.Name.ToLower()));
+                }
+                if (!string.IsNullOrEmpty(filter.Status))
+                {
+                    query = query.Where(d => d.Status.Name.ToLower().Contains(d.Status.Name.ToLower()));
+                }
+            }
+            return query;
+        }
+    }
+}
